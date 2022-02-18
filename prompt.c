0207f11b7a3c5bb27d9f37ffbcc0b224ae6a1c1a
Ariv Debnath <arivdebnath10@gmail.com>
2022-02-18 13:46:30 +0530

    updated preprocessor for WIN32

--- prompt.c
+++ prompt.c
@@ -1,8 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// for windows >> substitute readline && add_history function
+#ifdef _WIN32
+#include <string.h>
+
+static char buffer[2048];
+
+// substitute readline funtion
+char* readline(char* prompt){
+    fputs(prompt, stdout);
+    fgets(buffer, 2048, stdin);
+    char* buffer_str = malloc(strlen(buffer)+1);
+    strcpy(buffer_str, buffer);
+    buffer_str[strlen(buffer_str)-1] = '\0';
+    return buffer_str;
+}
+
+// substitute add_history function
+void add_history(char* sub_input){ };
+
+#else
+
 #include <editline/readline.h>
 #include <editline/history.h>
+
+#endif
 
 int main(int argc, char** argv){
 
