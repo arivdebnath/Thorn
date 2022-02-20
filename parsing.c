c3b695a24924d6cdfb807dfaab548e3ca18e41ad
Ariv Debnath <arivdebnath10@gmail.com>
2022-02-20 22:24:53 +0530

    added input evaluation

--- parsing.c
+++ parsing.c
@@ -29,6 +29,46 @@ void add_history(char *sub_input){};
 
 #endif
 
+int number_of_nodes(mpc_ast_t* t){
+    if(t->children_num == 0) {return 1;}
+    if(t-> children_num >= 1) {
+        int total = 1;
+        for(int i=0; i<t->children_num; i++){
+            total += number_of_nodes(t->children[i]);
+        }
+        return total;
+    }
+    return 0;
+}
+
+long eval_operation(long x, char* op, long y){
+    if(!strcmp(op, "+")){ return x+y; }
+    if(!strcmp(op, "-")){ return x-y; }
+    if(!strcmp(op, "*")){ return x*y; }
+    if(!strcmp(op, "/")){ return x/y; }
+    if(!strcmp(op, "%")){ return x%y; }
+    
+    return 0;
+}
+
+
+long eval(mpc_ast_t* t){
+    if(strstr( t->tag, "number")){
+        return atoi(t->contents);
+    }
+
+    char* op = t->children[1]->contents;
+
+    long x = eval(t->children[2]);
+
+    int j = 3;
+    while(strstr(t->children[j]->tag, "expr")){
+        x = eval_operation(x, op, eval(t->children[j]));
+        j++;
+    }
+    return x;
+}
+
 int main(int argc, char **argv)
 {
 
@@ -51,7 +91,7 @@ int main(int argc, char **argv)
     Number, Operator, Expr, Thorn);
 
     // displays basic information
-    puts("Thorn version 0.0.1");
+    puts("Thorn version 0.0.3");
     puts("Press Ctrl+C to Exit\n");
 
     while (1)
@@ -66,12 +106,13 @@ int main(int argc, char **argv)
         mpc_result_t r;
         if(mpc_parse("<stdin>", input, Thorn, &r)){
             mpc_ast_print(r.output);
+            printf("ans:%ld\n", eval(r.output));
             mpc_ast_delete(r.output);
         } else {
             mpc_err_print(r.error);
             mpc_err_delete(r.error);
         }
-
+        
         free(input);
     }
     mpc_cleanup(4, Number, Operator, Expr, Thorn);
